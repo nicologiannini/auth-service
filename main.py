75434d2b89ed8e8a0fec836f0176fe0ce13215d2
Nicolò Giannini <contact2nezz@yahoo.it>
2022-05-16 00:27:42 +0200

    service_manager() added

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import dbengine
-import utils
+import handler
 from flask import Flask, request, jsonify
-from classes import User, Response
+from classes import Response
 
 app = Flask(__name__)
 
-'''
-{
-    "email": "test@gmail.com",
-    "username": "admin",
-    "password": "1234",
-    "multi_factor": false
-}
-'''
-@app.route('/register/', methods=['POST'])
-def register():
+def service_manager(flow_handler):
     response = Response()
     try:
-        registration_data = request.get_json()
-        handle_registration(response, registration_data)
-    except TypeError as error:
+        data = request.get_json()
+        flow_handler(response, data)
+    except AttributeError as error:
         response.status_code = 400
         response.error = str(error)
     except ValueError as error:
         response.status_code = 401
         response.error = str(error)
-    except Exception:
+    except Exception as e:
         response.status_code = 500
         response.error = 'A generic error occurred on the server'
     return jsonify(response.__dict__), response.status_code
 
-@app.route('/access/', methods=['GET'])
-def access():
-    pass
 
-def handle_registration(response: Response, registration_data: dict):
-    if(registration_data and {'username', 'email', 'password', 'multi_factor'} <= registration_data.keys()):
-        new_user = User()
-        new_user.username = utils.validate_username(
-            registration_data['username'])
-        new_user.email = utils.validate_email(
-            registration_data['email'])
-        new_user.password = registration_data['password']
-        new_user.multi_factor = utils.validate_multi_factor(
-            registration_data['multi_factor'])
-        if dbengine.insert_user(new_user):
-            response.status_code = 200
-            response.body = f'{new_user.username} has been created.'
-        else:
-            raise Exception
-    else:
-        raise TypeError('Client sent an invalid request.')
+'''
+{
+    "username": "test",
+    "email": "test@gmail.com",
+    "password": "12345678",
+    "multi_factor": false
+}
+'''
+@app.route('/register/', methods=['POST'])
+def register():
+    return service_manager(handler.register_handler)
+
+
+'''
+{
+    "username": "test",
+    "password": "12345678"
+}
+'''
+@app.route('/access/', methods=['POST'])
+def access():
+    return service_manager(handler.access_handler)
+
+
+'''
+{
+    "username": "test",
+    "token": "123456"
+}
+'''
+@app.route('/token_login/', methods=['POST'])
+def token_login():
+    return service_manager(handler.token_login_handler)
 
 
 if __name__ == '__main__':
