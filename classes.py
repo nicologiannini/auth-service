cb0928eb6d15840544c21053bd9af96706f0d81a
Nicolò Giannini <contact2nezz@yahoo.it>
2022-05-17 00:33:21 +0200

    database setting + classes refinement

--- classes.py
+++ classes.py
@@ -2,11 +2,14 @@ from datetime import datetime
 
 class User():
     def __init__(self):
+        self._id = None
         self._username = None
         self._email = None
         self._password = None
-        self._multi_factor = False
+        self._multi_factor = None
         self._created_at = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        self._auth_token = None
+        self._token_exp_date = None
 
     @property
     def username(self):
@@ -28,6 +31,14 @@ class User():
     def created_at(self):
         return self._created_at
 
+    @property
+    def auth_token(self):
+        return self._auth_token
+
+    @property
+    def token_exp_date(self):
+        return self._token_exp_date
+
     @username.setter
     def username(self, username):
         self._username = username
@@ -44,9 +55,26 @@ class User():
     def multi_factor(self, is_enabled):
         self._multi_factor = is_enabled
 
+    @auth_token.setter
+    def auth_token(self, token):
+        self._auth_token = token
+
+    @token_exp_date.setter
+    def token_exp_date(self, date):
+        self._token_exp_date = date
+
+    def load(self, id, username, email, password, multi_factor, created_at, auth_token, token_exp_date):
+        self._id = int(id)
+        self._username = username
+        self._email = email
+        self._password = password
+        self._multi_factor = bool(multi_factor)
+        self._created_at = created_at
+        self._auth_token = auth_token
+        self._token_exp_date = token_exp_date
 
 class Response():
     def __init__(self):
         self.status_code = None
-        self.body = None
-        self.error = None
+        self.body = {}
+        self.error = ''
