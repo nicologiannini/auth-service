9f07b2bfb16050312577c509bd3da7f1b2904ed7
Nicolò Giannini <contact2nezz@yahoo.it>
2022-05-16 20:44:28 +0200

    logging methods added

--- utils.py
+++ utils.py
@@ -1,21 +1,77 @@
 import dbengine
 import re
+import random
+from flask_bcrypt import generate_password_hash, check_password_hash
+from datetime import datetime, timedelta
+from classes import User
 
-def validate_username(username):
-    if not dbengine.get_user_by_username(username) and len(username) <= 25:
-        return username
-    else:
-        raise ValueError("username is not valid.")
+ENABLE_LOG = True
+DT_FORMAT = '%d/%m/%Y %H:%M:%S'
 
-def validate_email(email):
+def validate_username(username: str):
+    if dbengine.get_user_by_username(username):
+        raise ValueError("This username is already registered.")
+    if not 4 <= len(username) <= 16:
+        raise ValueError("This username does not respect requirements.")
+    return username
+
+def validate_email(email: str):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    if re.fullmatch(regex, email) and len(email) <= 40:
-        return email
-    else:
-        raise ValueError("email is not valid.")
+    if not re.fullmatch(regex, email) and len(email):
+        raise ValueError("Apparently this is not an email.")
+    if dbengine.get_user_by_email(email):
+        raise ValueError("This email is already registered.")
+    return email
 
-def validate_multi_factor(is_enabled):
-    if isinstance(is_enabled, bool):
-        return is_enabled
-    else:
+def validate_password(password: str):
+    if not 8 <= len(password) <= 16:
+        raise ValueError("This password does not respect requirements.")
+    return generate_password_hash(password).decode('utf-8')
+
+def validate_multi_factor(is_enabled: bool):
+    if not isinstance(is_enabled, bool):
         raise ValueError('multi_factor should be boolean type.')
+    return 1 if is_enabled else 0
+
+def retrieve_user(username: str):
+    raw_user = dbengine.get_user_by_username(username)
+    if not raw_user:
+        raise ValueError('User not found.')
+    return raw_user
+    
+def verify_password(hashed_password: str, password_to_verify: str):
+    if not check_password_hash(hashed_password, password_to_verify):
+        raise ValueError('Wrong password.')
+
+def refresh_token(user: User):
+    new_token = ''.join([str(random.randint(0, 9)) for _ in range(6)])
+    new_token_exp_date = (datetime.now() + timedelta(minutes=5)).strftime(DT_FORMAT)
+    user.auth_token = new_token
+    user.token_exp_date = new_token_exp_date
+    if not dbengine.update_user_token_info(user):
+        raise Exception
+
+def verify_token(last_token: str, last_token_exp_date: str, token_to_verify: str):
+    last_token_exp_date = datetime.strptime(last_token_exp_date, DT_FORMAT)
+    if last_token != token_to_verify:
+        raise ValueError('Invalid token.')
+    if datetime.strptime(time_now(), DT_FORMAT) > last_token_exp_date:
+        raise ValueError('This token is expired.')
+
+def time_now() -> str:
+    now = datetime.now()
+    return now.strftime(DT_FORMAT)
+
+def log_execution(function):
+    def execution(*args, **kwargs):
+        if(ENABLE_LOG):
+            now_string = time_now()
+            function_name = function.__name__
+            print(f'{now_string} - {function_name} has been executed')
+        return function(*args, **kwargs)
+    return execution
+
+def log_detail(message: str) -> None:
+    if(ENABLE_LOG):
+        now_string = time_now()
+        print(f'{now_string} - {message}')
