e485386656c5c360e3ae25a7047b438cf2dab336
Nicolò Giannini <contact2nezz@yahoo.it>
2022-05-17 00:31:19 +0200

    db patch for testing

--- dbengine.py
+++ dbengine.py
@@ -2,19 +2,22 @@ import sqlite3
 import contextlib
 from classes import User
 
+DB = 'database.db'
+
 def execute_statement(statement, params=None) -> bool:
     try:
-        with contextlib.closing(sqlite3.connect('database.db')) as conn:
+        with contextlib.closing(sqlite3.connect(DB)) as conn:
             with conn:
                 with contextlib.closing(conn.cursor()) as cursor:
-                    cursor.execute(statement, params) if params else cursor.execute(statement)
+                    cursor.execute(
+                        statement, params) if params else cursor.execute(statement)
         return True
     except Exception as e:
         return False
 
 def execute_raw_query(query):
     try:
-        with contextlib.closing(sqlite3.connect('database.db')) as conn:
+        with contextlib.closing(sqlite3.connect(DB)) as conn:
             with conn:
                 with contextlib.closing(conn.cursor()) as cursor:
                     result = cursor.execute(query).fetchall()
@@ -22,17 +25,29 @@ def execute_raw_query(query):
     except Exception as e:
         return False
 
+def database_init():
+    create_users_table()
+
 def create_users_table():
     return execute_statement(
-        'CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, email TEXT, password TEXT, multi_factor TEXT, created_at TEXT, auth_token TEXT, token_exp_datetime TEXT)')
+        "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, email TEXT UNIQUE, password TEXT, multi_factor INTEGER, created_at TEXT, auth_token TEXT, token_exp_date TEXT)")
 
 def insert_user(user: User):
     return execute_statement("INSERT INTO users (username, email, password, multi_factor, created_at) VALUES (?, ?, ?, ?, ?)",
-                      (user.username, user.email, user.password, str(user.multi_factor), user.created_at))
+                             (user.username, user.email, user.password, str(user.multi_factor), user.created_at))
 
 def get_user_by_username(username):
-    result = execute_raw_query(f"SELECT * FROM users WHERE username = '{username}'")
-    return result[0] if len(result) > 0 else False
+    result = execute_raw_query(
+        f"SELECT * FROM users WHERE username = '{username}'")
+    return result[0] if result else False
+
+def get_user_by_email(email):
+    result = execute_raw_query(f"SELECT * FROM users WHERE email = '{email}'")
+    return result[0] if result else False
+
+def update_user_token_info(user: User):
+    return execute_statement(f"UPDATE users SET auth_token = '{user.auth_token}', token_exp_date = '{user.token_exp_date}' WHERE username = '{user.username}'")
+
 
 if __name__ == '__main__':
     create_users_table()
--- test.py
+++ test.py
@@ -4,16 +4,16 @@ from mock import patch
 
 class TestValidation(unittest.TestCase):
     def test_validate_username(self):
-        with patch("dbengine.get_user_by_username") as patched:
+        with patch('dbengine.get_user_by_username') as patched:
             patched.return_value = False
-            self.assertEqual(utils.validate_username("test"), "test")
-            self.assertRaises(ValueError, utils.validate_username, "")
+            self.assertEqual(utils.validate_username('test'), 'test')
+            self.assertRaises(ValueError, utils.validate_username, '')
         
     def test_validate_email(self):
-        with patch("dbengine.get_user_by_email") as patched:
+        with patch('dbengine.get_user_by_email') as patched:
             patched.return_value = False
-            self.assertEqual(utils.validate_email("test@gmail.com"), "test@gmail.com")
-            self.assertRaises(ValueError, utils.validate_email, "test@")
+            self.assertEqual(utils.validate_email('test@gmail.com'), 'test@gmail.com')
+            self.assertRaises(ValueError, utils.validate_email, 'test@')
             self.assertRaises(TypeError, utils.validate_email, 0)
     
     def test_validate_password(self):
@@ -24,9 +24,5 @@ class TestValidation(unittest.TestCase):
         self.assertEqual(utils.validate_multi_factor(True), 1)
         self.assertRaises(ValueError, utils.validate_multi_factor, 0)
 
-
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
