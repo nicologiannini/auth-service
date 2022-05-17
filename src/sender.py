414a4cded2e527ef5c6e7f970150c59036aba778
Nicolò Giannini <contact2nezz@yahoo.it>
2022-05-17 23:18:22 +0200

    email generator added

---
+++ src/sender.py
@@ -0,0 +1,22 @@
+import os
+import smtplib, ssl
+from utils import log_detail
+
+SERVER_MAIL = os.environ['SERVER_MAIL']
+SERVER_MAIL_PWD = os.environ['SERVER_MAIL_PWD']
+SERVER = ''
+
+def send_token_mail(receiver, token):
+    port = 465
+    smtp_server_domain_name = "smtp.gmail.com"
+    try:
+        ssl_context = ssl.create_default_context()
+        service = smtplib.SMTP_SSL(smtp_server_domain_name, port, context=ssl_context)
+        service.login(SERVER_MAIL, SERVER_MAIL_PWD)
+        sender = 'noreply@gmail.com'
+        subject = 'New token'
+        content = f'Token: {token} has been generated.'
+        service.sendmail(sender, [receiver], f'From: {sender}\nSubject: {subject}\n\n{content}')
+        service.quit()
+    except Exception as e:
+        log_detail(f'Token: {token} not sent')
