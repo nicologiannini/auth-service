9abd5ba1a4487723a7813fe92f062f30713551f8
Nicolò Giannini <nicogiannini@yahoo.com>
2022-07-05 19:14:33 +0200

    engine QueryType enum to better handle match for queries

--- src/engine.py
+++ src/engine.py
@@ -1,41 +1,13 @@
 import psycopg2
 import contextlib
+from enum import Enum
 from src.config import DB_CONFIG
 
 
-def execute_statement(query, params=None):
-    return _execute(query, params)
-
-
-def execute_fetchall(query, params=None):
-    return _execute(query, params, "fetchall")
-
-
-def execute_fetchone(query, params=None):
-    return _execute(query, params, "fetchone")
-
-
-def _execute(query, params=None, type=None, config=DB_CONFIG):
-    result = None
-    try:
-        with contextlib.closing(psycopg2.connect(**config)) as conn:
-            with conn:
-                with contextlib.closing(conn.cursor()) as cursor:
-                    cursor.execute(
-                        query, params) if params else cursor.execute(query)
-                    match type:
-                        case "fetchone":
-                            result = cursor.fetchone()
-                        case "fetchall":
-                            result = cursor.fetchall()
-                        case _:
-                            result = True
-                    conn.commit()
-    except Exception as e:
-        result = False
-    finally:
-        conn.close()
-        return result
+class QueryType(Enum):
+    Statement = 0
+    Fetchall = 1
+    Fetchone = 2
 
 
 def database_init():
@@ -51,3 +23,41 @@ def create_users_table():
                                     last_name VARCHAR(24) NOT NULL,
                                     email VARCHAR(36) NOT NULL,
                                     password VARCHAR(128) NOT NULL)""")
+
+
+def execute_statement(query, params=None):
+    return _execute(query, params, QueryType.Statement.value)
+
+
+def execute_fetchall(query, params=None):
+    return _execute(query, params, QueryType.Fetchall.value)
+
+
+def execute_fetchone(query, params=None):
+    return _execute(query, params, QueryType.Fetchone.value)
+
+
+def _execute(query, params=None, type=None, config=DB_CONFIG):
+    result = None
+    try:
+        conn = psycopg2.connect(**config)
+        with conn:
+            with contextlib.closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    query, params) if params else cursor.execute(query)
+                match type:
+                    case QueryType.Fetchone.value:
+                        result = cursor.fetchone()
+                    case QueryType.Fetchall.value:
+                        result = cursor.fetchall()
+                    case QueryType.Statement.value:
+                        result = True
+                    case _:
+                        result = None
+                conn.commit()
+    except Exception as e:
+        conn.rollback()
+        result = False
+    finally:
+        conn.close()
+        return result
